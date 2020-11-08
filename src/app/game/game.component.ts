cd893831105b3a97d5a70aaa361b6e7550bfe510
clement.esteve <clement.esteve@thalesdigital.io>
2020-11-08 23:18:39 +0100

    change user end chrono

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -107,13 +107,14 @@ export class GameComponent implements OnInit, OnDestroy {
 
   getChrono(x, chrono, snackbar) {
     if (chrono.started) {
-      let value = this.initialValue + 60*60*1000 - (new Date().valueOf()) + (new Date(chrono.begin).valueOf());
+      let value = this.initialValue + 60 * 60 * 1000 - (new Date().valueOf()) + (new Date(chrono.begin).valueOf());
       if (value <= 0) {
         chrono.started = false;
         value = this.initialValue;
         snackbar.open('End of chrono time, next player', '', { duration: 1000 });
         this.dataService.nextPlayer(this.gameId).subscribe(res => {
           console.log(res);
+          this.currentPlayer = res.user;
         });
       }
       this.chronovalue = Math.floor(value / 1000);
